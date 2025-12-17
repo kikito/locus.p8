acef97399e511272983a224e7d6a4bbcaa7867fc
kikito <kikito@gmail.com>
2025-12-17 17:19:44 +0100

    removed . Not needed since objects now occupy 1 cell all the time

--- locus.lua
+++ locus.lua
@@ -1,6 +1,6 @@
 _ENV.locus = function(size)
   size=size or 32
-  local cells,px,py,pool={},{},{},{}
+  local cells,cx,cy,pool={},{},{},{}
 
   local function frompool()
     local tbl=next(pool)
@@ -13,6 +13,25 @@ _ENV.locus = function(size)
 
   local function p2c(x,y)
     return (x\size)+1, (y\size)+1
+  end
+
+  local function addcell(cx,cy,obj)
+    local idx=cx|(cy>>>16)
+    if not cells[idx] then
+      cells[idx]=frompool()
+    end
+    cells[idx][obj]=true
+  end
+
+  local function delcell(cx,cy,obj)
+    local idx=cx|(cy>>>16)
+    local cell=cells[idx]
+    if cell then
+      cell[obj]=nil
+      if not next(cell) then
+        cells[idx],pool[cell]=nil,true
+      end
+    end
   end
 
   local function query_iter(l,t,r,b)
@@ -33,57 +52,31 @@ _ENV.locus = function(size)
     return obj
   end
 
-  local function each(op,p1,l,t,r,b)
-    local cell,idx
-    for cy=t,b do
-      for cx=l,r do
-        idx=cx|(cy>>>16)
-        if op=="add" and not cells[idx] then
-          cells[idx]=frompool()
-        end
-        cell=cells[idx]
-        if cell then
-          if op=="add" then
-            cell[p1]=true
-          elseif op=="del" then
-            cell[p1]=nil
-          elseif op=="free" and not next(cell) then
-            cells[idx],pool[cell]=nil,true
-          end
-        end
-      end
-    end
-  end
-
   return {
-    _px=px,_py=py,_p2c=p2c,_pool=pool,_cells=cells,_size=size,
+    _cx=cx,_cy=cy,_p2c=p2c,_pool=pool,_cells=cells,_size=size,
 
     add=function(obj,x,y)
-      px[obj],py[obj]=x,y
-      local cx,cy=p2c(x,y)
-      each("add",obj,cx,cy,cx,cy)
+      local cellx,celly=p2c(x,y)
+      cx[obj],cy[obj]=cellx,celly
+      addcell(cellx,celly,obj)
       return obj
     end,
 
     del=function(obj)
-      local x,y=assert(px[obj],"unknown object"),py[obj]
-      local cx,cy=p2c(x,y)
-      each("del",obj,cx,cy,cx,cy)
-      each("free",obj,cx,cy,cx,cy)
-      px[obj],py[obj]=nil,nil
+      local cellx,celly=assert(cx[obj],"unknown object"),cy[obj]
+      delcell(cellx,celly,obj)
+      cx[obj],cy[obj]=nil,nil
       return obj
     end,
 
     update=function(obj,x,y)
-      local x0,y0=assert(px[obj],"unknown object"),py[obj]
-      local cx0,cy0=p2c(x0,y0)
+      local cx0,cy0=assert(cx[obj],"unknown object"),cy[obj]
       local cx1,cy1=p2c(x,y)
       if cx0~=cx1 or cy0~=cy1 then
-        each("del",obj,cx0,cy0,cx0,cy0)
-        each("add",obj,cx1,cy1,cx1,cy1)
-        each("free",obj,cx0,cy0,cx0,cy0)
+        delcell(cx0,cy0,obj)
+        addcell(cx1,cy1,obj)
+        cx[obj],cy[obj]=cx1,cy1
       end
-      px[obj],py[obj]=x,y
     end,
 
     query=function(x,y,w,h)
